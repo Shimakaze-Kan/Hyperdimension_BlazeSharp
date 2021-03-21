c9caac2dfe9b73d9babbc332b0da22cf68cc96c2
Shimakaze-Kan <wiktormd@outlook.com>
2021-03-21 16:09:20 +0100

    Test api

---
+++ Hyperdimension_BlazeSharp/Server/Controllers/StaticImagesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hyperdimension_BlazeSharp.Server.Controllers
+{
+    [Route("staticimages")]
+    [ApiController]
+    public class StaticImagesController : Controller
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<string>>> GetImages()
+        {
+            return new List<string>() { "https://i.imgur.com/CXNopVm.png", "http://japoland.pl/blog/wp-content/uploads/400px-Ikku_Mikosi-nyudo.jpg" };
+        }
+    }
+}
