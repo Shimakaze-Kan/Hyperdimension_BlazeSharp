8ca7a7572be531a965c67ab79d6a8c381bc6eb0c
Shimakaze-Kan <wiktormd@outlook.com>
2021-03-29 22:02:41 +0200

    Add Users controller

---
+++ Hyperdimension_BlazeSharp/Server/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hyperdimension_BlazeSharp.Server.Controllers
+{
+    [Route("Users")]
+    [ApiController]
+    public class UsersController : Controller
+    {
+        List<Tuple<string,int>> list = new() { new("user1", 12), new("user1212", 122), new("user1000", 4) };
+
+        [HttpGet]
+        public async Task<ActionResult<List<Tuple<string, int>>>> GetUsers()
+        {
+            await System.Threading.Tasks.Task.Delay(1000);
+
+            return list;
+        }
+
+        [HttpGet("ranking")]
+        public async Task<ActionResult<List<Tuple<string, int>>>> GetRanking()
+        {
+            await System.Threading.Tasks.Task.Delay(1000);
+
+            return list.OrderByDescending(x => x.Item2).ToList();
+        }
+    }
+}
