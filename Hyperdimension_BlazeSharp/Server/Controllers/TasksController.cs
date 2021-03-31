6bd0b9bd95dadae9a2c5bfacc3d1c6a5c75ec158
Shimakaze-Kan <wiktormd@outlook.com>
2021-04-01 00:33:42 +0200

    Now tasks are received from database

--- Hyperdimension_BlazeSharp/Server/Controllers/TasksController.cs
+++ Hyperdimension_BlazeSharp/Server/Controllers/TasksController.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Hyperdimension_BlazeSharp.Shared.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Hyperdimension_BlazeSharp.Server.Controllers
 {
@@ -10,103 +12,30 @@ namespace Hyperdimension_BlazeSharp.Server.Controllers
     [ApiController]
     public class TasksController : Controller
     {
+        private readonly HblazesharpContext _db;
+
+        public TasksController(HblazesharpContext db)
+        {
+            _db = db;
+        }
+
         [HttpGet]
-        public async Task<ActionResult<List<Shared.Task>>> GetTasks()
+        public async Task<ActionResult<List<Tasks>>> GetTasks()
         {
-            await Task.Delay(1000);
-
-            return new List<Shared.Task>() { new() { Id = "853afe7a-349e-4354-b4a2-6f27d40af987",
-            ModuleId = "e7d97879-a1cd-47e0-ace2-2b6911e9bc89",
-            Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec pharetra dui",
-            Title = "Task nr 1",
-            InitialCode = @"using System;
-namespace BlazeSharpPlayground
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine(""Hello World!"");
-        }
-    }
-}",
-            TestCode = "",
-            Points = 1},
-
-            new() { Id = "9e4ef438-bb2f-44e7-bf27-07103c2c31cf",
-            ModuleId = "e7d97879-a1cd-47e0-ace2-2b6911e9bc89",
-            Description = "Make the program output Hell World! instead of Hello World!",
-            Title = "Task nr 2",
-            InitialCode = @"using System;
-namespace BlazeSharpPlayground
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine(""Hello World!"");
-        }
-    }
-}",
-            TestCode = "Hell World!",
-            Points = 1}};
+            return await _db.Tasks.ToListAsync();
         }
 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Shared.Task>> GetSpecyficTask(string id)
+        [HttpGet("{id:Guid}")]
+        public async Task<ActionResult<Tasks>> GetSpecyficTask(Guid id)
         {
-            await Task.Delay(1000);
+            var task = await _db.Tasks.FirstOrDefaultAsync(x => x.Id == id);
 
-            if (id == "853afe7a-349e-4354-b4a2-6f27d40af987")
+            if(task is null)
             {
-                return new Shared.Task()
-                {
-                    Id = "853afe7a-349e-4354-b4a2-6f27d40af987",
-                    ModuleId = "e7d97879-a1cd-47e0-ace2-2b6911e9bc89",
-                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec pharetra dui",
-                    Title = "Task nr 1",
-                    InitialCode = @"using System;
-namespace BlazeSharpPlayground
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine(""Hello World!"");
-        }
-    }
-}",
-                    TestCode = "",
-                    Points = 1
-                };
+                return NotFound();
             }
 
-            if (id == "9e4ef438-bb2f-44e7-bf27-07103c2c31cf")
-            {
-                return new Shared.Task()
-                {
-                    Id = "9e4ef438-bb2f-44e7-bf27-07103c2c31cf",
-                    ModuleId = "e7d97879-a1cd-47e0-ace2-2b6911e9bc89",
-                    Description = "Make the program output Hell World! instead of Hello World!",
-                    Title = "Task nr 2",
-                    InitialCode = @"using System;
-namespace BlazeSharpPlayground
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine(""Hello World!"");
-        }
-    }
-}",
-                    TestCode = "Hell World!",
-                    Points = 1
-                };
-            }
-
-
-            return NotFound();
+            return task;
         }
     }
 }
