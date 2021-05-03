aac16eb95738dad2044b11039a9b8cab6699725d
Shimakaze-Kan <wiktormd@outlook.com>
2021-05-03 16:16:52 +0200

    Add profile tab menu

---
+++ Hyperdimension_BlazeSharp/Client/wwwroot/js/show-profile-tab.js
@@ -0,0 +1,17 @@
+﻿export function showProfileTab(navTabId, tabPaneId) {
+
+    const navTabs = document.querySelectorAll('#nav-tab > .nav-link');
+
+    for (let i = 0; i < navTabs.length; i++) {
+        navTabs[i].className = navTabs[i].className.replace(" active", "");
+    }
+
+    const tabPanes = document.getElementsByClassName("tab-pane");
+
+    for (let i = 0; i < tabPanes.length; i++) {
+        tabPanes[i].className = tabPanes[i].className.replace(" show active", "");
+    }
+
+    document.getElementById(navTabId).className += " active";
+    document.getElementById(tabPaneId).className += " show active";
+}
