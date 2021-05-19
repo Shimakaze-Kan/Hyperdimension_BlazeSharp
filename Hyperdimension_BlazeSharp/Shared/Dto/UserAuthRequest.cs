30b93046f3681ccd84ca92b8335001160e8e3e7d
Shimakaze-Kan <wiktormd@outlook.com>
2021-05-20 01:02:09 +0200

    Add new authentication dto

---
+++ Hyperdimension_BlazeSharp/Shared/Dto/UserAuthRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hyperdimension_BlazeSharp.Shared.Dto
+{
+    public class UserAuthRequest
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(5, ErrorMessage = "Password has to be at least 5 characters long")]
+        public string Password { get; set; }
+    }
+}
---
+++ Hyperdimension_BlazeSharp/Shared/Dto/UserAuthResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hyperdimension_BlazeSharp.Shared.Dto
+{
+    public class UserAuthResult
+    {
+        public string Token { get; set; }
+        public string Email { get; set; }
+    }
+}
