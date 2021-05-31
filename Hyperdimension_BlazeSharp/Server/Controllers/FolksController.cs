75b608fb5c45a758b9f62d83b05d63fb77dffdc8
Shimakaze-Kan <wiktormd@outlook.com>
2021-05-31 23:09:32 +0200

    Create folks controller

---
+++ Hyperdimension_BlazeSharp/Server/Controllers/FolksController.cs
@@ -0,0 +1,64 @@
+﻿using Hyperdimension_BlazeSharp.Shared.Dto;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hyperdimension_BlazeSharp.Server.Controllers
+{
+    [Route("Folks")]
+    [ApiController]
+    public class FolksController : Controller
+    {
+        private readonly HblazesharpContext _db;
+
+        public FolksController(HblazesharpContext db)
+        {
+            _db = db;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<FolkStory>>> GetFolks()
+        {
+            var result = await _db.FolkStories
+                .Select(x => new FolkStory()
+                {
+                    ImgUrl = x.ImageUrl,
+                    Story = x.Story,
+                    Title = x.Title
+                })
+                .ToListAsync();
+
+            if(result is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+
+        [HttpGet("/{id:guid}")]
+        public async Task<ActionResult<FolkStory>> GetFolk(Guid id)
+        {
+            var result = await _db.FolkStories
+                .Where(x => x.Id == id)
+                .Select(x => new FolkStory()
+                {
+                    ImgUrl = x.ImageUrl,
+                    Story = x.Story,
+                    Title = x.Title
+                })
+                .FirstOrDefaultAsync();
+
+            if(result is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+    }
+}
---
+++ Hyperdimension_BlazeSharp/Shared/Dto/FolkStory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hyperdimension_BlazeSharp.Shared.Dto
+{
+    public class FolkStory
+    {
+        public string Title { get; set; }
+        public string Story { get; set; }
+        public string ImgUrl { get; set; }
+    }
+}
