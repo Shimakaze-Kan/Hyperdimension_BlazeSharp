f342017faa8e2eeb1dc9009099099216469d4c1f
Shimakaze-Kan <wiktormd@outlook.com>
2021-06-08 23:41:06 +0200

    Create base repository

---
+++ Hyperdimension_BlazeSharp/Server/Repositories/BaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hyperdimension_BlazeSharp.Server.Repositories
+{
+    public abstract class BaseRepository
+    {
+        protected readonly HblazesharpContext _hblazesharpContext;
+
+        public BaseRepository(HblazesharpContext hblazesharpContext)
+        {
+            _hblazesharpContext = hblazesharpContext;
+        }
+    }
+}
