157e62de95dfe345d0e7af338dac22c222bc4537
Shimakaze-Kan <wiktormd@outlook.com>
2021-07-01 15:00:46 +0200

    Create new comment dialog component

--- Hyperdimension_BlazeSharp/Shared/Dto/CommentCreateRequest.cs
+++ Hyperdimension_BlazeSharp/Shared/Dto/CommentCreateRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace Hyperdimension_BlazeSharp.Shared.Dto
     public class CommentCreateRequest
     {
         public Guid TaskId { get; set; }
+        [MinLength(5, ErrorMessage = "Comment have to be at least 5 characters long")]
         public string Text { get; set; }
         public bool AddLastSubmittedVersion { get; set; }
     }
